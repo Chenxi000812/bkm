7ee659320b7d10236f1006738ab1609f4ca5e75b
晨曦 <984077246@qq.com>
2022-06-07 20:49:56 +0800

    第一次提交

---
+++ src/com/bookmanager/servlet/LoginServlet.java
@@ -0,0 +1,24 @@
+package com.bookmanager.servlet; /**
+ * @Classname ${NAME}
+ * @Description TODO
+ * @Date 2022/6/7 20:44
+ * @Created by 晨曦
+ */
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+import java.io.IOException;
+
+@WebServlet(name = "LoginServlet", value = "/LoginServlet")
+public class LoginServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
---
+++ src/com/bookmanager/util/MysqlUtil.java
@@ -0,0 +1,11 @@
+package com.bookmanager.util;
+
+/**
+ * @Classname MysqlUtil
+ * @Description TODO
+ * @Date 2022/6/7 20:43
+ * @Created by 晨曦
+ */
+public class MysqlUtil {
+
+}
